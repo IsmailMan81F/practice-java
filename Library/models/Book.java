bb70e63be90aff436c7a265f28ab7fe5aa58f2ee
IsmailMan81F <ismailman81fmeg@gmail.com>
2026-02-08 11:01:59 +0100

    complete Book class

--- Library/models/Book.java
+++ Library/models/Book.java
@@ -1,4 +1,43 @@
 package models;
+
 public class Book {
-    
+    private String title;
+    private String author;
+    private String isbn;
+
+    public static int totalBooks;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        totalBooks++;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void printInfo() {
+        String info = "Book : " + this.title + ", Author : " + this.author + ", isbn : " + this.isbn
+                + "\nTotal number of books : " + totalBooks;
+        System.out.println(info);
+    }
+
 }
